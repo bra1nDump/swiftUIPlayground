39c4633172fba94e12bd0abf7cc968e277bc7bcb
kostas-mac-client <kostas2003de@icloud.com>
2019-12-12 12:52:48 -0800

    Initial Commit

---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by Konstantin Dubovitskiy on 12/12/19.
+//  Copyright © 2019 Kirill Dubovitskiy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
